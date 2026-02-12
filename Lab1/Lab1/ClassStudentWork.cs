d0db5f3d98c9d9e600d6c00c1a4c9a17be5589e5
Dima <kuznetzoffdm1t@yandex.com>
2026-02-12 17:14:32 +0700

    first

---
+++ Lab1/Lab1/ClassStudentWork.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    internal class ClassStudentWork
+    {
+        public string Name { get; set; }
+        public string NameOfWork { get; set; }
+        public DateTime DateOfWork { get; set; }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Студент: {Name}");
+            Console.WriteLine($"Название работы: \"{NameOfWork}\" ");
+            Console.WriteLine($"Дата выдачи: {DateOfWork.ToShortDateString()}");
+        }
+        public ClassStudentWork(string name, string nameOfWork, DateTime date)
+        {
+            Name = name;
+            NameOfWork = nameOfWork;
+            DateOfWork = date;
+        }
+        ~ClassStudentWork()
+        {
+        }
+    }
+}
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rawString, name, work;
+            DateTime dateOfWork;
+
+            Console.WriteLine("Введите данные о теме работы студента в следующем формате:");
+            Console.WriteLine("\"Имя студента (строка)\" \"название темы (строка)\" дата выдачи");
+            rawString = Console.ReadLine();
+
+            string[] words = rawString.Split(new char[] { '"' }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> splitStrings = words.ToList();
+            splitStrings.RemoveAt(1);
+
+            name = splitStrings[0];
+            work = splitStrings[1];
+
+            words = splitStrings[2].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            dateOfWork = DateTime.Parse(words[0]);
+
+            ClassStudentWork studentWork = new ClassStudentWork(name, work, dateOfWork);
+            studentWork.DisplayInfo();
+        }
+    }
+}
