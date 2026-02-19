7003c10d2ba71c9dcf94e4f8c184b804741dd065
Dima <kuznetzoffdm1t@yandex.com>
2026-02-19 09:04:11 +0700

    update1

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -9,6 +9,19 @@ namespace Lab_1
 {
     internal class Program
     {
+        static string trueString(string s)
+        {
+            string[] strings = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string result = "";
+            for(int i = 0; i < strings.Length; i++)
+            {
+                result += strings[i];
+                if (i != strings.Length - 1) result += " ";
+            }
+            return result;
+        }
+
+
         static void Main(string[] args)
         {
             string rawString, name, work;
@@ -16,14 +29,20 @@ namespace Lab_1
 
             Console.WriteLine("Введите данные о теме работы студента в следующем формате:");
             Console.WriteLine("\"Имя студента (строка)\" \"название темы (строка)\" дата выдачи");
-            rawString = Console.ReadLine();
-
+            rawString = "    \" Ivanov Ivan\"           \"IS Decanat\"   23.12.2023";// Console.ReadLine();
+            List<string> list = new List<string>();
             string[] words = rawString.Split(new char[] { '"' }, StringSplitOptions.RemoveEmptyEntries);
 
-            name = words[0];
-            work = words[2];
+            foreach (string word in words) 
+            {
+                string[] subword = word.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (subword.Length != 0) list.Add(word);
+            }
 
-            words = words[3].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            name = trueString(list[0]);
+            work = trueString(list[1]);
+
+            words = list[2].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
             dateOfWork = DateTime.Parse(words[0]);
 
